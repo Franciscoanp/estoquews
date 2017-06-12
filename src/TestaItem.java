2cdc0eb6b8fc984cdb8715ea40a7b9fd53b56638
Francisco Pereira <francisco.anpereira@gmail.com>
2017-06-12 15:59:28 -0300

    atualizando projeto soap

---
+++ src/TestaItem.java
@@ -0,0 +1,11 @@
+import br.com.caelum.estoque.modelo.item.Item;
+
+public class TestaItem {
+
+	public static void main(String[] args) {
+		
+		Item item = new Item("abcd", "iphon7", "celulares", 5);
+		System.out.println(item);
+		
+	}
+}
