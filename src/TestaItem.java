ee87af44231010412e7eb908644711b518801873
Francisco Pereira <francisco.anpereira@gmail.com>
2017-06-12 16:26:48 -0300

    adicionando classe TestaItem e gitignore

---
+++ src/TestaItem.java
@@ -0,0 +1,11 @@
+import br.com.caelum.estoque.modelo.item.Item;
+
+public class TestaItem {
+
+	public static void main(String[] args) {
+		
+		Item item = new Item("abc", "abc", "aaa", 4);
+		System.out.println(item);
+		
+	}
+}
